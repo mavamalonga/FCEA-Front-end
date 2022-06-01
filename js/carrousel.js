cfcbc8fb95c54cf921c7283eb926231ce0d9f030
Alpha <mavamalonga.alpha@gmail.com>
2022-06-01 04:01:21 +0200

    reviewCode

--- js/carrousel.js
+++ js/carrousel.js
@@ -3,6 +3,6 @@ $('#caroussel').slick({
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 5000,
-    prevArrow:"<img class='a-left control-c prev slick-prev' src='../img/left.png'>",
-    nextArrow:"<img class='a-right control-c next slick-next' src='../img/next.png'>"
+    prevArrow:"<img class='a-left control-c prev slick-prev' src='../img/arrow-28-24.png'>",
+    nextArrow:"<img class='a-right control-c next slick-next' src='../img/arrow-28-24.png'>"
 });
---
+++ js/gallery.js
@@ -0,0 +1,6 @@
+$('#gallery').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow:"<img class='a-left control-c prev slick-prev' src='../img/left.png'>",
+    nextArrow:"<img class='a-right control-c next slick-next' src='../img/next.png'>"
+});
